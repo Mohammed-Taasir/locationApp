76aa009775e3f858341074c4c4aa2105a21c939d
Taasir <taasir22029@iiitd.ac.in>
2023-03-08 18:36:22 +0530

    this app detects location

--- app/src/main/java/com/example/locationbytaasir/MainActivity.java
+++ app/src/main/java/com/example/locationbytaasir/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.locationbytaasir;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // first commit
+    TextView Latextview, Longtextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Latextview = findViewById(R.id.latitude);
+        Longtextview = findViewById(R.id.longitude);
     }
 }
